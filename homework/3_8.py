8ec3f6d8f512186484fd2978b7ef52100abc9b51
ChengMioa <50311063+ChengMioa@users.noreply.github.com>
2025-05-19 17:19:06 +0800

    更新 PYthon心得.md

---
+++ homework/3_8.py
@@ -0,0 +1,25 @@
+di_dian = ['ShanHai','BeiJing','TianJin','NanJing']
+print()
+
+print(di_dian)
+print()
+
+print(sorted(di_dian))
+print(di_dian)
+print()
+
+print(sorted(di_dian,reverse=True))
+print(di_dian)
+print()
+
+di_dian.reverse()
+print(di_dian)
+
+di_dian.reverse()
+print(di_dian)
+print()
+
+di_dian.sort()
+print(di_dian)
+di_dian.sort(reverse=True)
+print(di_dian)
