a67271fe8af954aa2983ec8f88c449bc7cfe5234
ChengMioa <50311063+ChengMioa@users.noreply.github.com>
2025-05-31 17:11:13 +0800

    修复代码格式，确保输出每位魔术师的姓名后有换行

--- sample_code/magicians.py
+++ sample_code/magicians.py
@@ -1,3 +1,12 @@
 magicians =['alice','david','carolina']
 for magician in magicians:
     print(magician)
+
+print()
+
+for magician in magicians:
+    print(f'{magician.title()},thant was a great trick!')
+    print(f"I can't wait to see your next trick,{magician.title()}\n")
+
+print("Thanks\n")
+
