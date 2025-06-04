97608b8d229b50e8f3bcd4c9ec8bd599107a3f7c
ChengMioa <50311063+ChengMioa@users.noreply.github.com>
2025-06-04 12:12:25 +0800

    新增披萨和动物示例，展示喜欢的食物和宠物

---
+++ homework/4_1.py
@@ -0,0 +1,11 @@
+pizzas = ['必胜客','麦当劳','肯德基']
+for pizza in pizzas:
+    print(f"我喜欢吃{pizza}牌子的披萨")
+
+print ()
+
+print("我喜欢吃这些牌子的披萨:")
+for pizza in pizzas:
+    print(f"                        {pizza}")
+print()
+print('我喜欢披萨')
