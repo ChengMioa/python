b8bdc25a07e3f59e053cf0a8e672c41de742e600
ChengMioa <50311063+ChengMioa@users.noreply.github.com>
2025-06-07 11:24:43 +0800

    新增示例，展示元组

---
+++ homework/4_13.py
@@ -0,0 +1,9 @@
+foods =('牛肉面','蛋炒饭','青椒肉丝','大排鸡','羊肉面',)
+print("10点餐馆的食物：")
+for food in foods:
+    print(food)
+
+foods =('牛肉面','番茄鸡蛋面','大排面','鸡腿面','羊肉面',)
+print("\n11点餐馆的食物：")
+for food in foods:
+    print(food)
