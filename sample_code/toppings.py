e95d85a8084dcbd139c0328c6e6bcc09ac0d3687
ChengMioa <50311063+ChengMioa@users.noreply.github.com>
2025-06-22 20:19:15 +0800

    if - elif - else

--- sample_code/toppings.py
+++ sample_code/toppings.py
@@ -1,9 +1,9 @@
 requested_toppings=['mushrooms','extra cheese']
 if 'mushrooms' in requested_toppings:
     print("Adding mushrooms")
-if 'pepperoni' in requested_toppings:
+elif 'pepperoni' in requested_toppings:
     print('Adding pepperoni')
-if 'extra cheese'in requested_toppings:
+elif 'extra cheese'in requested_toppings:
     print("Adding extra cheese")
 
 print("\n Finished making your pizza")
