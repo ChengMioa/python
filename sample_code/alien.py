f6c6e569b1f6f9587067c1f74b1805186e9c7360
ChengMioa <50311063+ChengMioa@users.noreply.github.com>
2025-07-04 19:53:35 +0800

    字典键对值

--- sample_code/alien.py
+++ sample_code/alien.py
@@ -1,3 +1,12 @@
 alien_0 = {'color':'green','poits':5}
 print(alien_0['color'])
 print(alien_0['poits'])
+#增加键值对
+print(alien_0)
+alien_0['x_poits'] = 0
+alien_0['y_piits'] = 25
+print(alien_0)
+#修改键对值
+print(f"old color is {alien_0['color']}")
+alien_0['color'] = 'red'
+print(f"Now color is {alien_0['color']}")
---
+++ sample_code/favorite_languages.py
@@ -0,0 +1,7 @@
+favorite_languages = {
+    'jen' : 'python',
+    'jie' : 'c',
+    'sarah' : 'rust',
+}
+language = favorite_languages['jie'].title()
+print(f"Jie like {language}")
